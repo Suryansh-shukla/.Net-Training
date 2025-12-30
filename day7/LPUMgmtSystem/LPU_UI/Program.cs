478ca06b7cd9b9ba6418818d6f4237c6658624ea
Suryansh Shukla <ssuryansh678@gmail.com>
2025-12-30 14:12:00 +0530

    Day10 Commit

--- day7/LPUMgmtSystem/LPU_UI/Program.cs
+++ day7/LPUMgmtSystem/LPU_UI/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+using LPU_Common;
+using LPU_DAL;
+using LPU_Entity;
+namespace LPU_UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericClass<int> genericClass = new GenericClass<int>();
+            int a = 10;
+            int b = 20;
+            Console.WriteLine($"Before Swap: a = {a}, b = {b}");
+            genericClass.SwapMe(ref a, ref b);
+            Console.WriteLine($"After Swap: a = {a}, b = {b}");
+        }
+    }
+}
