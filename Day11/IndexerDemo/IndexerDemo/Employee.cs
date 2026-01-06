14ec90c0bb325233bfdf4088e1f97884ae7989c4
Suryansh Shukla <ssuryansh678@gmail.com>
2026-01-06 16:14:53 +0530

    Commit of Customers data

---
+++ Day11/IndexerDemo/IndexerDemo/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndexerDemo
+{
+    class Employee
+    {
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public int BSalary { get; set; }
+        public static Employee operator +(Employee emp1, Employee emp2)
+        {
+            Employee empObj = new Employee();
+            empObj.BSalary = emp1.BSalary + emp2.BSalary;
+            return empObj;
+        }
+    }
+}
---
+++ Day11/IndexerDemo/IndexerDemo/OperatorOverloadingDemo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndexerDemo
+{
+    class OperatorOverloadingDemo
+    {
+        
+        static void Main(string[] args)
+        {
+            Employee emp1=new Employee() { EmpID=101,EmpName="Alok",BSalary=45000};
+            Employee emp2=new Employee() { EmpID=102,EmpName="Rakshas",BSalary=55000};
+            Employee emp3=new Employee() { EmpID=103,EmpName="Buddy",BSalary=60000};
+            Employee empObj = emp1 + emp2 + emp3;
+            System.Console.WriteLine($"Total Salary is: {empObj.BSalary}");
+        }
+    }
+}
