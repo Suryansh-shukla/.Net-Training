47aea34efb293d922970a0214c41bb95f67124bf
Suryansh Shukla <ssuryansh678@gmail.com>
2026-01-07 10:49:38 +0530

    New Windows From App Event Handler

--- Day11/IndexerDemo/IndexerDemo/Employee.cs
+++ Day11/IndexerDemo/IndexerDemo/Employee.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace IndexerDemo
 {
-    class Employee
+    internal class Employee
     {
         public int EmpID { get; set; }
         public string EmpName { get; set; }
@@ -14,6 +14,14 @@ namespace IndexerDemo
             Employee empObj = new Employee();
             empObj.BSalary = emp1.BSalary + emp2.BSalary;
             return empObj;
+        }
+
+        public static Employee operator +(Employee emp1, Employee emp2)
+        {
+            return new Employee
+            {
+                BSalary = (emp1?.BSalary ?? 0) + (emp2?.BSalary ?? 0)
+            };
         }
     }
 }
