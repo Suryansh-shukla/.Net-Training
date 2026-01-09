ae79430ffe0eb286b86484f046b553e223ed914b
Suryansh Shukla <ssuryansh678@gmail.com>
2026-01-09 12:35:04 +0530

    Commit of 09/01/2026 for event handling

---
+++ Day11/EventDelegations/EventDelegations/EventDemo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Transactions;
+
+namespace EventDelegations
+{
+    interface IBank
+    {
+        public List<string> GenStatement();
+        bool OpenAct(string Details);
+    }
+    interface RBI
+    {
+
+    }
+    public class HDFC : IBank
+    {
+        public delegate void Transaction(int actNo, string date, int amount);
+        public event Debit;
+        public event Credit;
+        public List<string> GenStatement()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        bool IBank.OpenAct(string Details)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Axis : IBank
+    {
+        List<string> IBank.GenStatement()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IBank.OpenAct(string Details)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class HDFCJalandhar : HDFC
+    {
+
+    }
+    class EventDemo
+    {
+        public static void EventDemoMain()
+        {
+
+        }
+    }
+}
