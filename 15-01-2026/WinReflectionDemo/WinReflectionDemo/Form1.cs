97e4adcd9ba55e0a3b0667355337e540da118eaa
Suryansh Shukla <ssuryansh678@gmail.com>
2026-01-15 10:18:55 +0530

    Commits of .Net Technology done on 15-01-2026 on Event handling and learning about GUI in .Net FrameWork

---
+++ 15-01-2026/WinReflectionDemo/WinReflectionDemo/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinReflectionDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string FileName = openFileDialog1.FileName;
+            Assembly assemblyObj= Assembly.LoadFrom(FileName);
+            Type[] types = assemblyObj.GetTypes();
+            p.ReflectAll(types[0]);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+        }
+    }
+}
