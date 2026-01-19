618eb046e91ec6e0eda0df3ac0ae041410658594
Suryansh Shukla <ssuryansh678@gmail.com>
2026-01-19 10:44:56 +0530

    Commits of Problems on Top Brains

---
+++ TopBrains_Questions/Multiplication_Table.cs
@@ -0,0 +1,21 @@
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n=Convert.ToInt32(Console.ReadLine());
+        int upto=Convert.ToInt32(Console.ReadLine());
+        int[] row=new int[upto];
+        if(upto>0)
+        {
+           for(int i=1;i<=upto;i++)
+        {
+            row[i-1]=n*i;
+        }
+        }
+        foreach(int num in row)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
