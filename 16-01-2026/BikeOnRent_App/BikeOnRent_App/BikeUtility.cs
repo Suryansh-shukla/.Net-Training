b87f51329930d2b7b26fc516522d45b6cb857b0c
Suryansh Shukla <ssuryansh678@gmail.com>
2026-01-31 14:14:17 +0530

    Bike rent assignment

---
+++ 16-01-2026/BikeOnRent_App/BikeOnRent_App/BikeUtility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeOnRent_App
+{
+    internal class BikeUtility
+    {
+        public void AddBikeDetails(string model,string brand, int pricePerDay)
+        {
+            int bikeID=Program.bikeDetails.Count+1;
+            Bike newBike = new Bike
+            {
+                Model = model,
+                Brand = brand,
+                PricePerDay = pricePerDay
+            };
+            Program.bikeDetails.Add(bikeID, newBike);
+        }
+        public SortedDictionary<string, List<Bike>> GroupBikesByBrand()
+        {
+            return new SortedDictionary<string, List<Bike>>(
+                Program.bikeDetails.Values
+                .GroupBy(bike => bike.Brand)
+                .ToDictionary(group => group.Key, group => group.ToList())
+            );
+        }
+    }
+}
---
+++ 16-01-2026/BikeOnRent_App/BikeOnRent_App/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeOnRent_App
+{
+    public class Program
+    {
+        internal readonly static SortedDictionary<int, Bike> bikeDetails=new SortedDictionary<int, Bike>();
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("1. Add Bike Details");
+                Console.WriteLine("2. Group Bikes by Brand");
+                Console.WriteLine("3. Exit");
+                Console.WriteLine("Enter your choice:");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter Bike Model:");
+                        string model = Console.ReadLine();
+                        Console.WriteLine("Enter Bike Brand:");
+                        string brand = Console.ReadLine();
+                        Console.WriteLine("Enter Price Per Day:");
+                        int pricePerDay = Convert.ToInt32(Console.ReadLine());
+                        BikeUtility bikeUtility = new BikeUtility();
+                        bikeUtility.AddBikeDetails(model, brand, pricePerDay);
+                        Console.WriteLine("Bike details added successfully.");
+                        break;
+                    case 2:
+                        BikeUtility bikeUtil = new BikeUtility();
+                        var groupedBikes = bikeUtil.GroupBikesByBrand();
+                        foreach (var brandGroup in groupedBikes)
+                        {
+                            Console.WriteLine($"Brand: {brandGroup.Key}");
+                            foreach (var bike in brandGroup.Value)
+                            {
+                                Console.WriteLine($"  Model: {bike.Model}, Price Per Day: {bike.PricePerDay}");
+                            }
+                        }
+                        break;
+                    case 3:
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+
+                }
+            }
+        }
+    }
+}
