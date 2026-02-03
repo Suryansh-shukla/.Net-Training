a260e608cd555b395858361cab315b4a3ceb1853
Suryansh Shukla <ssuryansh678@gmail.com>
2026-02-03 15:28:42 +0530

    Factory Robot Hazard Question of TopBrains

---
+++ Questions/FactoryRobotHazardAnalyzer/FactoryRobotHazardAnalyzer/RobotSafetyException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryRobotHazardAnalyzer
+{
+    public class RobotSafetyException:Exception
+    {
+        public RobotSafetyException(string message) : base(message)
+        {
+        }
+    }
+}
