4197bc3c3baffa25f1b4b09786414c2c7a1fe4e8
Suryansh Shukla <ssuryansh678@gmail.com>
2026-02-24 09:54:42 +0530

    Commiting changes done to MVC Web Application Demo for practice

---
+++ ASP.Net/MVCWebAppDemo/MVCWebAppDemo/Controllers/DummyController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWebAppDemo.Controllers
+{
+    public class DummyController : Controller
+    {
+        // GET: Dummy
+        public ActionResult Index()
+        {
+            List<string> nameList =new  List<string>{ "Alok", "Riya", "Rajat" };
+            ViewBag.Names = nameList;
+            return View(nameList);
+        }
+    }
+}
---
+++ ASP.Net/MVCWebAppDemo/MVCWebAppDemo/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCWebAppDemo.Models
+{
+    public class Student
+    {
+        public int RollNo { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+    }
+}
