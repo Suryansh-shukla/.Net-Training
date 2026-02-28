016d5152728180ed7aa663f64318733ef1ff7f17
Suryansh Shukla <ssuryansh678@gmail.com>
2026-02-28 11:50:53 +0530

    Application Project Re-creation due to sudden system downtime  and crashing application

---
+++ Sprint/HospitalManagementSolution/Core/Interfaces/IDoctorService.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IDoctorService:IRepository<Entities.Doctor>
+    {
+        void AddDoctor(Doctor doctor);
+        List<Doctor> GetDoctors();
+        Doctor GetDoctorById(int Id);
+        void UpdateDoctor(int Id, Doctor doctor);
+        void DeleteDoctor(int Id);
+
+    }
+}
---
+++ Sprint/HospitalManagementSolution/Core/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IRepository<T>
+    {
+        void Add(T entity);
+        List<T> GetAll();
+        T GetById(int id);
+        void Update(int Id,T entity);
+        void Delete(int Id);
+    }
+}
