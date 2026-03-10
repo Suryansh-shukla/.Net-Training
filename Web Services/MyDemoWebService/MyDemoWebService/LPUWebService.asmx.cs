18e6c1a16c209ef3e998aa8f440a49b50dbe5766
Suryansh Shukla <ssuryansh678@gmail.com>
2026-03-10 10:11:55 +0530

    Learning about Web Services

---
+++ Web Services/MyDemoWebService/MyDemoWebService/LPUWebService.asmx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace MyDemoWebService
+{
+    /// <summary>
+    /// Summary description for LPUWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class LPUWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+        [WebMethod]
+        public int AddMe(int num1,int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
