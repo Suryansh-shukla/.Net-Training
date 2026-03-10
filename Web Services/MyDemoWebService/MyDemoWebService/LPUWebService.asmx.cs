fdeca2de2460d8e37525f72794f55e804bfd1a65
Suryansh Shukla <ssuryansh678@gmail.com>
2026-03-10 11:43:36 +0530

    Learning Database First approach of Entity Framework

--- Web Services/MyDemoWebService/MyDemoWebService/LPUWebService.asmx.cs
+++ Web Services/MyDemoWebService/MyDemoWebService/LPUWebService.asmx.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace MyDemoWebService
 {
@@ -26,6 +28,44 @@ namespace MyDemoWebService
         public int AddMe(int num1,int num2)
         {
             return num1 + num2;
+        }
+        [WebMethod]
+        public Product[] ShowAllProducts()
+        {
+            //Connection
+            SqlConnection  conn=new SqlConnection();
+            conn.ConnectionString= "Server=.\\sqlexpress;Database=LPU_Db;Trusted_Connection=True;TrustServerCertificate=true;";
+            conn.Open();
+
+            //Command
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "select * from Products";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = conn;
+
+            //DataReader
+            SqlDataReader reader = cmd.ExecuteReader();
+            Product[] ProdList = null;
+            DataTable dt=new DataTable();
+            dt.Load(reader);
+            if (dt.Rows.Count>0)
+            {
+                ProdList = new Product[dt.Rows.Count];
+            }
+            int count = 0;
+            foreach (DataRow item in dt.Rows)
+            {
+               Product pObj=new Product();
+                pObj.ProdId = Convert.ToInt32(item["ProdId"]);
+                pObj.Name = item["Name"].ToString();
+                pObj.Category = item["Category"].ToString();
+                pObj.Price = Convert.ToDecimal(item["Price"]);
+                pObj.Description = item["Desc"].ToString();
+                ProdList[count] = pObj;
+                count++;
+            }
+            conn.Close();
+            return ProdList;
         }
     }
 }
